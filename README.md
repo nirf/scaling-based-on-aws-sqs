6e0c362b6f670e149fea7dce9bd6f132d8b2c03f
Nir.Feigelshtein <nir.feigelshtein@forcepoint.com>
2022-01-02 17:48:42 +0200

    remove the profile from serverless.yml

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Deep dive on how to scale instances based on AWS SQS
 # cloudinit log path
 /var/log/cloud-init-output.log
+# Running instructions
+`
+/terraform/>terraform apply
+/backlog-per-instance-calculator/> serverless deploy --aws-profile dev
+`
--- backlog-per-instance-calculator/src/scripts/publishMessages.js
+++ backlog-per-instance-calculator/src/scripts/publishMessages.js
@@ -5,29 +5,45 @@ const sqs = new AWS.SQS({
     region: 'eu-west-1'
 })
 
-for (let i = 0; i < 250; i++) {
-    sqs.sendMessage({
-        MessageAttributes: {
-            "Title": {
-                DataType: "String",
-                StringValue: "The Whistler"
-            },
-            "Author": {
-                DataType: "String",
-                StringValue: "John Grisham"
-            },
-            "WeeksOn": {
-                DataType: "Number",
-                StringValue: "6"
-            }
-        },
-        MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-        QueueUrl: "https://sqs.eu-west-1.amazonaws.com/399971917915/my_queue"
-    }, (err, data) => {
-        if (err) {
-            console.log("Error", err)
-        } else {
-            console.log("Success", data.MessageId)
+
+sendMessages(10, 60 * 1000, 20).then(() => console.log('Finished'))
+
+async function sendMessages(batchSize, sleepInMs, rounds) {
+    for (let j = 0; j < rounds; j++) {
+        for (let i = 0; i < batchSize; i++) {
+            sqs.sendMessage({
+                MessageAttributes: {
+                    "Title": {
+                        DataType: "String",
+                        StringValue: "The Whistler"
+                    },
+                    "Author": {
+                        DataType: "String",
+                        StringValue: "John Grisham"
+                    },
+                    "WeeksOn": {
+                        DataType: "Number",
+                        StringValue: "6"
+                    }
+                },
+                MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+                QueueUrl: "https://sqs.eu-west-1.amazonaws.com/399971917915/my_queue"
+            }, (err, data) => {
+                if (err) {
+                    console.log("Error", err)
+                } else {
+                    console.log("Success", data.MessageId)
+                }
+            })
         }
+        await sleep(sleepInMs)
+        console.log(`>>>>>>>Finished round ${j}<<<<<<<<<<`)
+    }
+}
+
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
     })
 }
